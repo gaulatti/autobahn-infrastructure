e03b6114606459dd60acbce3292f67493ae8617f
Javier Godoy <rotvulpix@gmail.com>
2024-06-13 18:49:31 -0400

    Initial commit

---
+++ test/dress-you-up.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as DressYouUp from '../lib/dress-you-up-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/dress-you-up-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new DressYouUp.DressYouUpStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
