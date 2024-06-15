00d692e4d8263ceaaa88df10d2850841fe303f53
Javier Godoy <rotvulpix@gmail.com>
2024-06-15 10:01:39 -0400

    fix: Shortening stack name

--- bin/dress-you-up.ts
+++ bin/dress-you-up.ts
@@ -4,7 +4,7 @@ import * as cdk from 'aws-cdk-lib';
 import { DressYouUpStack } from '../lib';
 
 const app = new cdk.App();
-new DressYouUpStack(app, 'DressYouUpStack', {
+new DressYouUpStack(app, 'DressYouUp', {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
