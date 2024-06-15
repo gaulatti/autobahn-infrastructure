e9625df9874571b46830c339880a148a955fedc0
Javier Godoy <rotvulpix@gmail.com>
2024-06-15 11:03:20 -0400

    chore: Adding demo data

--- lib/worker/secrets/index.ts
+++ lib/worker/secrets/index.ts
@@ -9,7 +9,7 @@ import { StringParameter } from 'aws-cdk-lib/aws-ssm';
 const createParameters = (stack: Stack) => {
   const apiKeyParameter = new StringParameter(stack, `${stack.stackName}ApiKey`, {
     parameterName: `${stack.stackName}ApiKey`,
-    stringValue: 'TBD',
+    stringValue: 'e9963d14-ca0d-4eee-aaf2-3f08159f5c5e',
   });
 
   return { apiKeyParameter };
