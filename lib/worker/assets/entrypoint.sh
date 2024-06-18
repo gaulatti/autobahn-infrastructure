c042fcc582dbfa195454d882d8f0fe6b123a08ad
Javier Godoy <rotvulpix@gmail.com>
2024-06-18 13:41:02 -0400

    chore: Adding more flags to chrome/lighthouse

--- lib/worker/assets/entrypoint.sh
+++ lib/worker/assets/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-mkdir ./.lighthouseci && lighthouse --chrome-flags="--no-sandbox --disable-dev-shm-usage" $TARGET_PARAMETER --output=json --output-path=./.lighthouseci/lhr-123456789.json  && lhci upload --config=./lhci.config.js
+mkdir ./.lighthouseci && lighthouse --chrome-flags="--no-sandbox --disable-dev-shm-usage --headless --remote-debugging-port=9222" $TARGET_PARAMETER --output=json --output-path=./.lighthouseci/lhr-123456789.json  && lhci upload --config=./lhci.config.js
