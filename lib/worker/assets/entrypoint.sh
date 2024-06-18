83244494976cabc3d87bebf5173dc99c90d6ede3
Javier Godoy <rotvulpix@gmail.com>
2024-06-18 13:53:36 -0400

    wip: Attempting to make chrome discovrable by lighthouse

--- lib/worker/assets/entrypoint.sh
+++ lib/worker/assets/entrypoint.sh
@@ -1,3 +1,13 @@
 #!/bin/bash
 
-mkdir ./.lighthouseci && lighthouse --chrome-flags="--no-sandbox --disable-dev-shm-usage --headless --remote-debugging-port=9222" $TARGET_PARAMETER --output=json --output-path=./.lighthouseci/lhr-123456789.json  && lhci upload --config=./lhci.config.js
+# Start Chrome in the background
+google-chrome --headless --no-sandbox --disable-dev-shm-usage --remote-debugging-port=9222 &
+
+# Wait for Chrome to start
+sleep 5
+
+# Create the directory for Lighthouse CI results
+mkdir -p ./.lighthouseci
+
+# Run Lighthouse using the pre-started Chrome instance
+lighthouse --port=9222 --chrome-flags="--no-sandbox --disable-dev-shm-usage --headless --remote-debugging-port=9222" $TARGET_PARAMETER --output=json --output-path=./.lighthouseci/lhr-123456789.json  && lhci upload --config=./lhci.config.js
