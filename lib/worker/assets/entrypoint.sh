1b376b46292bf0638d641a4c047783c119d295ca
Javier Godoy <rotvulpix@gmail.com>
2024-06-18 13:20:14 -0400

    wip: Attempting to collect with lighthouse and post to lhci

--- lib/worker/assets/entrypoint.sh
+++ lib/worker/assets/entrypoint.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-lhci autorun --config=./lhci.config.js
+mkdir ./.lighthouseci && lighthouse --chrome-flags="--no-sandbox --disable-dev-shm-usage" $TARGET_PARAMETER --output=json --output-path=./.lighthouseci/lhr-123456789.json  && lhci upload --config=./lhci.config.js
