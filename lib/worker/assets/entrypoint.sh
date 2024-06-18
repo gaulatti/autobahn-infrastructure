f5cb8b857c531a1ca701a85263dfa62d0f8dea91
Javier Godoy <rotvulpix@gmail.com>
2024-06-18 14:02:57 -0400

    wip: Adding logs to make sure we're reaching upload point

--- lib/worker/assets/entrypoint.sh
+++ lib/worker/assets/entrypoint.sh
@@ -10,4 +10,8 @@ sleep 5
 mkdir -p ./.lighthouseci
 
 # Run Lighthouse using the pre-started Chrome instance
-lighthouse --port=9222 --chrome-flags="--no-sandbox --disable-dev-shm-usage --headless --remote-debugging-port=9222" $TARGET_PARAMETER --output=json --output-path=./.lighthouseci/lhr-123456789.json  && lhci upload --config=./lhci.config.js
+lighthouse --port=9222 --chrome-flags="--no-sandbox --disable-dev-shm-usage --headless --remote-debugging-port=9222" $TARGET_PARAMETER --output=json --output-path=./.lighthouseci/lhr-123456789.json
+
+echo "Lighthouse Complete, time to upload"
+
+lhci upload --config=./lhci.config.js
