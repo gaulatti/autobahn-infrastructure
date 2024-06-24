061b4aa5c77401541d4239b2af4c6dc5c650de7a
Javier Godoy <rotvulpix@gmail.com>
2024-06-24 12:52:10 -0400

    win: CW Alarms

--- lib/common/utils/index.ts
+++ lib/common/utils/index.ts
@@ -1,3 +1,12 @@
 const camelToKebab = (str: string) => str.replace(/([a-z0–9])([A-Z])/g, '$1-$2').toLowerCase();
 
-export { camelToKebab };
+const kebabToCamel = (str: string) => {
+  return str.replace(/-./g, (match) => match.charAt(1).toUpperCase());
+};
+
+const capitalize = (str: string) => {
+  if (str.length === 0) return str;
+  return str.charAt(0).toUpperCase() + str.slice(1);
+};
+
+export { camelToKebab, kebabToCamel, capitalize };
