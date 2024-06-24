317c8ce25bedb69ec63640f790660b98c6aaca96
Javier Godoy <rotvulpix@gmail.com>
2024-06-23 23:54:36 -0400

    win: Dashboard with CW Metrics

--- lib/observability/dashboard/index.ts
+++ lib/observability/dashboard/index.ts
@@ -1,7 +1,87 @@
-import { Stack } from 'aws-cdk-lib';
+import { Duration, Stack } from 'aws-cdk-lib';
+import { Dashboard, GraphWidget, Metric, PeriodOverride } from 'aws-cdk-lib/aws-cloudwatch';
 
 const createDashboard = (stack: Stack) => {
-    // coming soon
+  /**
+   * Represents the CloudWatch dashboard.
+   */
+  const dashboard = new Dashboard(stack, `${stack.stackName}Dashboard`, {
+    dashboardName: `${stack.stackName}Dashboard`,
+    defaultInterval: Duration.days(7),
+  });
+
+  /**
+   * Represents the First Content Paint (p90) widget.
+   */
+  const firstContentPaintWidget = new GraphWidget({
+    title: 'First Content Paint (p90)',
+    left: [
+      new Metric({
+        namespace: stack.stackName,
+        metricName: 'first-contentful-paint',
+        dimensionsMap: {
+          Viewport: 'desktop',
+          Stage: 'prod',
+        },
+        region: 'us-east-1',
+        statistic: 'p90',
+        period: Duration.hours(1),
+      }),
+      new Metric({
+        namespace: stack.stackName,
+        metricName: 'first-contentful-paint',
+        dimensionsMap: {
+          Viewport: 'mobile',
+          Stage: 'prod',
+        },
+        region: 'us-east-1',
+        statistic: 'p90',
+        period: Duration.hours(1),
+      }),
+    ],
+    width: 12,
+    height: 6,
+  });
+
+  /**
+   * Represents the Largest Content Paint (p90) widget.
+   */
+  const largestContentPaintWidget = new GraphWidget({
+    title: 'Largest Content Paint (p90)',
+    left: [
+      new Metric({
+        namespace: stack.stackName,
+        metricName: 'largest-contentful-paint',
+        dimensionsMap: {
+          Viewport: 'desktop',
+          Stage: 'prod',
+        },
+        region: 'us-east-1',
+        statistic: 'p90',
+        period: Duration.hours(1),
+      }),
+      new Metric({
+        namespace: stack.stackName,
+        metricName: 'largest-contentful-paint',
+        dimensionsMap: {
+          Viewport: 'mobile',
+          Stage: 'prod',
+        },
+        region: 'us-east-1',
+        statistic: 'p90',
+        period: Duration.hours(1),
+      }),
+    ],
+    width: 12,
+    height: 6,
+  });
+
+  /**
+   * Add the widgets to the dashboard.
+   */
+  dashboard.addWidgets(firstContentPaintWidget, largestContentPaintWidget);
+
+  return { dashboard };
 };
 
 export { createDashboard };
--- lib/observability/index.ts
+++ lib/observability/index.ts
@@ -1,6 +1,7 @@
 import { Stack } from 'aws-cdk-lib';
 import { createBucket } from './storage';
 import { createProcessingLambda } from './functions/processing';
+import { createDashboard } from './dashboard';
 
 const createObservabilityInfrastructure = (stack: Stack) => {
   /**
@@ -12,6 +13,11 @@ const createObservabilityInfrastructure = (stack: Stack) => {
    * Process Lambda
    */
   const { processingLambda } = createProcessingLambda(stack, observabilityBucket);
+
+  /**
+   * Dashboard
+   */
+  const { dashboard } = createDashboard(stack);
 
   return { observabilityBucket };
 };
