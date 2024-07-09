8c645fe3ec53566944657e0ca32928e9501b9afd
Javier Godoy <rotvulpix@gmail.com>
2024-07-09 12:37:00 -0400

    wip #5: Getting secret from Secrets Manager

--- lib/observability/functions/dal.src.ts
+++ lib/observability/functions/dal.src.ts
@@ -1,5 +1,19 @@
-const main = (event: any) => {
-  console.log(event);
-  // This is the main function
-  console.log('Hello, world!');
+import { SecretsManagerClient, GetSecretValueCommand } from '@aws-sdk/client-secrets-manager';
+
+const client = new SecretsManagerClient({});
+
+const main = async (event: any) => {
+  try {
+    /**
+     * Retrieve the secret value from the Database secret.
+     */
+    const command = new GetSecretValueCommand({ SecretId: process.env.DATABASE_SECRET });
+    const response = await client.send(command);
+    const secretValue = response.SecretString;
+
+  } catch (error) {
+    console.error('Error retrieving secret:', error);
+  }
 };
+
+export { main };
