99c70a2d86a3c288114c92dc8fcf030949f5d7eb
Javier Godoy <rotvulpix@gmail.com>
2024-08-15 19:11:33 -0400

    wip: Adding failure function call to worker entrypoint

--- lib/worker/assets/entrypoint.sh
+++ lib/worker/assets/entrypoint.sh
@@ -1,5 +1,29 @@
 #!/bin/bash
 
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Set NODE_OPTIONS as a global environment variable
+export NODE_OPTIONS="--max_old_space_size=14336"
+
+# Function to trigger Lambda on failure
+trigger_lambda_on_failure() {
+    echo "Script failed, triggering Lambda..."
+
+    # Create a JSON payload with $UUID and $MODE
+    PAYLOAD=$(jq -n \
+      --arg uuid "$UUID" \
+      --arg mode "$MODE" \
+      --arg error "FAILED" \
+      '{status: $error, uuid: $uuid, mode: $mode}')
+
+    # Invoke the Lambda function with the payload
+    aws lambda invoke --function-name $ERROR_LAMBDA_ARN --payload "$PAYLOAD" /dev/null
+}
+
+# Set up a trap to catch errors and trigger the Lambda function
+trap 'trigger_lambda_on_failure' ERR
+
 # Start Chrome in the background
 google-chrome --headless --no-sandbox --disable-dev-shm-usage --remote-debugging-port=9222 &
 
