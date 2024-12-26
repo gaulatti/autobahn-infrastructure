25de2405429dbfa81bfe84f56a341732e13683bc
Javier Godoy <rotvulpix@gmail.com>
2024-12-25 22:50:25 -0500

    wip: Porting Lighthouse and Fargate

---
+++ lib/worker/assets/entrypoint.sh
@@ -0,0 +1,83 @@
+#!/bin/bash
+
+# Exit immediately if a command exits with a non-zero status
+set -e
+
+# Set NODE_OPTIONS as a global environment variable
+export NODE_OPTIONS="--max_old_space_size=14336"
+
+# Function to trigger Lambda on failure
+trigger_lambda_on_failure() {
+    echo "Script failed, triggering Lambda..."
+
+    # Create a JSON payload with $UUID and $MODE
+    PAYLOAD=$(jq -n \
+      --arg uuid "$UUID" \
+      --arg mode "$MODE" \
+      --arg url "$TARGET_PARAMETER" \
+      --arg preset "$PRESET_FLAG" \
+      --arg error "FAILED" \
+      --argjson retries "${RETRIES:-0}" \
+      '{status: $error, uuid: $uuid, mode: $mode, url: $url, preset: $preset, retries: ($retries + 1)}')
+
+    # Invoke the Lambda function with the payload
+    aws lambda invoke \
+      --function-name $ERROR_LAMBDA_ARN \
+      --payload "$PAYLOAD" \
+      --cli-binary-format raw-in-base64-out \
+      /dev/null
+}
+
+# Set up a trap to catch errors and trigger the Lambda function
+trap 'trigger_lambda_on_failure' ERR
+
+# Start Chrome in the background
+google-chrome --headless --no-sandbox --disable-dev-shm-usage --remote-debugging-port=9222 &
+
+# Wait for Chrome to start
+sleep 5
+
+# Run Lighthouse using the pre-started Chrome instance
+lighthouse \
+  --port=9222 \
+  --chrome-flags="--no-sandbox --disable-dev-shm-usage --headless --remote-debugging-port=9222" \
+  $TARGET_PARAMETER \
+  $PRESET_FLAG \
+  --output=json \
+  --output-path=./$UUID.json \
+  --throttling-method=devtools \
+  --throttling.cpuSlowdownMultiplier=1 \
+  --throttling.throughputKbps=0 \
+  --throttling.requestLatencyMs=0 \
+  --max-wait-for-load=180000
+
+echo "Lighthouse Complete, time to upload"
+
+# Upload the Lighthouse report to the S3 bucket with a random UUID filename
+aws s3 cp ./$UUID.json s3://$BUCKET_NAME/$UUID.$MODE.json
+
+echo "Upload Complete"
+
+if [[ -n "$SEGUE_ARN" ]]; then
+    echo "Invoking SEGUE Lambda..."
+
+    # Create the payload for the SEGUE Lambda invocation
+    SEGUE_PAYLOAD=$(jq -n \
+      --arg action "SEGUE" \
+      --arg bucketName "$BUCKET_NAME" \
+      --arg uuid "$UUID" \
+      --arg mode "$MODE" \
+      --arg playlistId "$UUID" \
+      '{bucketName: $bucketName, action: $action, uuid: $uuid, mode: $mode}')
+
+    # Invoke the SEGUE Lambda function with the payload
+    aws lambda invoke \
+      --function-name "$SEGUE_ARN" \
+      --payload "$SEGUE_PAYLOAD" \
+      --cli-binary-format raw-in-base64-out \
+      /dev/null
+
+    echo "SEGUE Lambda invoked successfully"
+else
+    echo "SEGUE_ARN not set. Skipping SEGUE Lambda invocation."
+fi
---
+++ lib/worker/fargate/index.ts
@@ -0,0 +1,61 @@
+import { Stack } from 'aws-cdk-lib';
+import { AwsLogDriver, ContainerImage, CpuArchitecture, FargateTaskDefinition, OperatingSystemFamily } from 'aws-cdk-lib/aws-ecs';
+import { NodejsFunction } from 'aws-cdk-lib/aws-lambda-nodejs';
+import { LogGroup, RetentionDays } from 'aws-cdk-lib/aws-logs';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+
+/**
+ * Creates a Fargate task definition with the specified stack and secrets.
+ * @param stack - The CloudFormation stack.
+ * @returns The Fargate task definition.
+ */
+const createFargateTask = (stack: Stack, observabilityBucket: Bucket, failureHandlerLambda: NodejsFunction) => {
+  /**
+   * Represents the CloudWatch log group.
+   */
+  const logGroup = new LogGroup(stack, `${stack.stackName}WorkerLogGroup`, {
+    logGroupName: `${stack.stackName}Worker`,
+    retention: RetentionDays.ONE_WEEK,
+  });
+
+  /**
+   * Represents the log driver used by the Fargate service.
+   */
+  const logDriver = new AwsLogDriver({
+    logGroup,
+    streamPrefix: 'Lighthouse',
+  });
+
+  /**
+   * Represents the Fargate task definition.
+   */
+  const fargateTaskDefinition = new FargateTaskDefinition(stack, `${stack.stackName}WorkerFargateTask`, {
+    cpu: 8192,
+    memoryLimitMiB: 16384,
+    runtimePlatform: { cpuArchitecture: CpuArchitecture.X86_64, operatingSystemFamily: OperatingSystemFamily.LINUX },
+  });
+
+  /**
+   * Grants the Fargate task permission to write to the
+   * observability bucket and invoke the failure handler Lambda.
+   */
+  observabilityBucket.grantWrite(fargateTaskDefinition.taskRole);
+  failureHandlerLambda.grantInvoke(fargateTaskDefinition.taskRole);
+
+  /**
+   * Adds a container to the Fargate task definition.
+   */
+  fargateTaskDefinition.addContainer(`${stack.stackName}WorkerFargateContainer`, {
+    containerName: `${stack.stackName}Worker`,
+    image: ContainerImage.fromAsset('./lib/observability/assets'),
+    logging: logDriver,
+    environment: {
+      ERROR_LAMBDA_ARN: failureHandlerLambda.functionArn,
+      BUCKET_NAME: observabilityBucket.bucketName,
+    },
+  });
+
+  return { fargateTaskDefinition };
+};
+
+export { createFargateTask };
